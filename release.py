2ca5402ab788ab78872cc1cfdc767bd5d9d4cb5b
Алексей Бузанов <abuzanov@techops.ru>
2017-03-28 19:21:26 +0300

    task number from pull request

--- release.py
+++ release.py
@@ -5,14 +5,15 @@ import subprocess
 import sys
 import argparse
 from collections import namedtuple
-from functools import lru_cache
 
+from cached_property import cached_property
 from jira import JIRA
+from github import Github
 
 TASK_PROJECT = 'TAN'
 RELEASE_PROJECT = 'RELEASE'
 TASK_RE = re.compile(r'({}-\d+)\s'.format(TASK_PROJECT), flags=re.U | re.I)
-PR_RE = re.compile(r'\((#\d+)\)', flags=re.U | re.I)
+PR_RE = re.compile(r'\(#(\d+)\)', flags=re.U | re.I)
 
 GetTaskResponse = namedtuple('GetTaskResponse', ('tasks', 'left_pulls'))
 
@@ -21,7 +22,12 @@ def git_fetch():
     subprocess.check_call('git fetch', shell=True, stdout=subprocess.PIPE)
 
 
-def get_related_tasks(task_key, origin_branch):
+def get_pr_task(api_client, pr):
+    pull = api_client.github_repository.get_pull(pr)
+    return TASK_RE.findall(pull.title)
+
+
+def get_related_tasks(task_key, origin_branch, api_client):
     git_fetch()
     commit_messages = subprocess.check_output(
         'git log origin/{}..origin/{} --pretty=%B'.format(
@@ -35,8 +41,10 @@ def get_related_tasks(task_key, origin_branch):
         tasks = TASK_RE.findall(line)
         pull_request = PR_RE.search(line)
         if pull_request and not tasks:
-            pr_number = pull_request.group(1)
-            left_pulls.add(pr_number)
+            pr_number = int(pull_request.group(1))
+            tasks = get_pr_task(api_client=api_client, pr=pr_number)
+            if not tasks:
+                left_pulls.add(pr_number)
         if not pull_request:
             continue
         all_tasks |= {task.upper() for task in tasks}
@@ -47,12 +55,12 @@ def get_related_tasks(task_key, origin_branch):
     )
 
 
-def make_links(jira, task_key, related_keys):
+def make_links(api_client, task_key, related_keys):
     for related_key in related_keys:
-        jira.create_issue_link('relates to', task_key, related_key)
+        api_client.jira.create_issue_link('relates to', task_key, related_key)
 
 
-def make_release_task(jira, extra_fields):
+def make_release_task(api_client, extra_fields):
     if not extra_fields:
         extra_fields = {}
 
@@ -61,7 +69,7 @@ def make_release_task(jira, extra_fields):
         extra_fields['components'] = [{'name': component_name}]
 
     summary = '{} release'.format(component_name) if component_name else 'Release'
-    issue = jira.create_issue(
+    issue = api_client.jira.create_issue(
         project=RELEASE_PROJECT,
         summary=summary,
         issuetype={'name': 'Task'},
@@ -85,18 +93,65 @@ def make_release_branch(task_key):
     )
 
 
-@lru_cache()
-def get_jira_api(jira_server, jira_user, jira_password):
-    assert jira_server
-    assert jira_user
-    assert jira_password
+class API:
+    def __init__(self, arguments):
+        self._args = arguments
 
-    return JIRA({
-        'server': jira_server
-    }, basic_auth=(jira_user, jira_password))
+    @cached_property
+    def jira(self):
+        assert self._args.jira_server
+        assert self._args.jira_user
+        assert self._args.jira_password
+
+        return JIRA({
+            'server': self._args.jira_server
+        }, basic_auth=(self._args.jira_user, self._args.jira_password))
+
+    @cached_property
+    def github(self):
+        assert self._args.github_user
+        assert self._args.github_password
+        return Github(self._args.github_user, self._args.github_password)
+
+    @cached_property
+    def github_repository(self):
+        assert self._args.github_repo
+        return self.github.get_repo(self._args.github_repo)
 
 
-if __name__ == '__main__':
+def run(commands, api_client, origin_branch, jira_task_extra, task_key=None):
+    if 'all' in commands or 'make-task' in commands:
+        task_key = make_release_task(api_client, jira_task_extra)
+        print('Made new task: {}'.format(task_key))
+
+    if 'all' in commands or 'make-branch' in commands:
+        assert task_key
+        make_release_branch(task_key)
+        print('Made release branch')
+
+    if 'all' in commands or 'make-relations' in commands:
+        assert task_key
+        relations = get_related_tasks(task_key, origin_branch, api_client=api_client)
+
+        if relations.left_pulls:
+            sys.stderr.write('Pull requests without tasks: {}\n'.format(
+                ', '.join(map(str, relations.left_pulls))
+            ))
+
+        if not relations.tasks:
+            sys.stderr.write('Did not find related tasks')
+            exit(1)
+
+        make_links(api_client, task_key, relations.tasks)
+
+        print('Made links from {} to {}'.format(
+            task_key, ', '.join(relations.tasks)))
+
+        if relations.left_pulls:
+            exit(1)
+
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'commands',
@@ -111,44 +166,18 @@ if __name__ == '__main__':
         '--jira-task-extra',
         type=lambda value: json.loads(value),
         help='Something like \'{"component": "Backend"}\'')
+    parser.add_argument('--github-user')
+    parser.add_argument('--github-password')
+    parser.add_argument('--github-repo')
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    task_key = args.release_task
 
-    if 'all' in args.commands or 'make-task' in args.commands:
-        task_key = make_release_task(
-            get_jira_api(args.jira_server, args.jira_user, args.jira_password),
-            args.jira_task_extra)
-        print('Made new task: {}'.format(task_key))
-
-    if 'all' in args.commands or 'make-branch' in args.commands:
-        assert task_key
-        make_release_branch(task_key)
-        print('Made release branch')
-
-    if 'all' in args.commands or 'make-relations' in args.commands:
-        assert task_key
-        relations = get_related_tasks(task_key, args.origin_branch)
-
-        if relations.left_pulls:
-            sys.stderr.write('Pull requests without tasks: {}\n'.format(
-                ', '.join(relations.left_pulls)
-            ))
-
-        if not relations.tasks:
-            sys.stderr.write('Did not find related tasks')
-            exit(1)
-
-        make_links(
-            get_jira_api(args.jira_server, args.jira_user, args.jira_password),
-            task_key,
-            relations.tasks)
-
-        print('Made links from {} to {}'.format(
-            task_key,
-            ', '.join(relations.tasks)
-        ))
-
-        if relations.left_pulls:
-            exit(1)
+if __name__ == '__main__':
+    args = parse_args()
+    run(
+        commands=args.commands,
+        api_client=API(args),
+        origin_branch=args.origin_branch,
+        jira_task_extra=args.jira_task_extra,
+        task_key=args.release_task)
